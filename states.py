96e77a21328eff7acbcd17f6f298af12dd136ec2
QueenMab <makkanzi@gmail.com>
2014-02-17 13:48:16 -0500

    Create states.py

---
+++ states.py
@@ -0,0 +1,16 @@
+states_list = "Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District Of Columbia","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","PALAU","Pennsylvania","PUERTO RICO","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"
+abbrevs_list= "AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PW","PA","PR","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"
+
+filename = "states.html"
+
+output = []
+
+output.append ("<select>")
+for state, abbrevs in zip (states_list, abbrevs_list):
+	output.append ("<option value={0}>{1}</option>".format(abbrevs, state))
+output.append ("</select>") 
+
+output="".join(output)
+
+with open (filename,"a") as states_file:
+	states_file.write(output)
